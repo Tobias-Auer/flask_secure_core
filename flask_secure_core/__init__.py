ccbbc0665d4a63765261987a12bd017cebce0757
Tobias Auer <tobias.auer@aol.com>
2025-08-20 21:09:29 +0200

    started init.py

--- flask_secure_core/__init__.py
+++ flask_secure_core/__init__.py
@@ -1,1 +1,21 @@
-# Initialisierung + init_app Funktion
+# __init__.py
+from .blueprints import bp
+
+
+_connection_manager = None
+
+def get_connection_manager():
+    return _connection_manager
+
+def _init_app(app):
+    app.register_blueprint(bp)
+
+def init_fsl(app, db_type='postgres', db_name=None, db_user=None, db_password=None, db_host='localhost', db_port=5432, minconn=3, maxconn=10, recreate_db=False):
+    global _connection_manager
+    _init_app(app)
+
+    if db_type == 'postgres':
+        from .db.DBConnectionManagerPostgres import DBConnectionManager
+        _connection_manager = DBConnectionManager(db_host=db_host, db_port=db_port, db_name=db_name, db_user=db_user, db_password=db_password, minconn=minconn, maxconn=maxconn, recreate_db=recreate_db)
+    else:
+        raise ValueError(f"Unsupported database type: {db_type}")

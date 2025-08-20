ca4ecbc28240fdd114eb601cb1fcd1174b18528c
Tobias Auer <tobias.auer@aol.com>
2025-08-20 21:10:20 +0200

    using init function in example file

--- examples/main-app.py
+++ examples/main-app.py
@@ -1,24 +1,41 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import flask_secure_core
+
 app = Flask(__name__)
 
+flask_secure_core.init_fsl(
+    app,
+    db_name="fsl",
+    db_user="fsl",
+    db_password="fsl"
+)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "Welcome to the Main Example App!"
+
 
 @app.route("/ex1")
 def example1():
     return "Only available when the user is logged in."
 
+
 @app.route("/ex2")
 def example2():
     return "Only available when the user has staff privileges."
 
+
 @app.route("/ex3")
 def example3():
     return "Only available when the user is an admin."
+
 
 @app.route("/ex4")
 def example4():
@@ -26,4 +43,4 @@ def example4():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
